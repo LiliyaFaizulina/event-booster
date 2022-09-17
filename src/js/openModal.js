c153d3fc003eb460110412cf5b1126a42051c3a8
BigSiCorp <si.kocorp@gmail.com>
2022-09-17 23:28:30 +0300

    MW_JS_03

--- src/js/openModal.js
+++ src/js/openModal.js
@@ -8,14 +8,20 @@ const eventTimedate = document.querySelector('.js-when');
 const eventPointPlace = document.querySelector('.js-where');
 const eventPointPlaceAddress = document.querySelector('.js-where-place');
 const eventFace = document.querySelector('.js-who');
+const firstPriceInfo = document.querySelector('.js-first-price');
+const firstPriceText = document.querySelector('.js-first-price .js-price-text');
+const secondPriceItem = document.querySelector('.js-second-priceItem');
+const secondPriceText = document.querySelector(
+  '.js-second-price .js-price-text'
+);
+const buyTicketBtn = document.querySelectorAll('.modal__list-btn');
 
 export function addToModalContent(id) {
-  console.log(id);
   infoObj.getEvent(id).then(response => {
-    console.log(response.data._embedded.events[0]);
-    const { images, name, dates, _embedded } =
+    //console.log(response.data._embedded.events[0]);
+    const { images, name, dates, _embedded, priceRanges, url } =
       response.data._embedded.events[0];
-    smallPic.src = images[0].url;
+    smallPic.src = images[8].url;
     poster.src = images[8].url;
     eventInfo.textContent = name;
     eventTimedate.textContent = `${
@@ -25,7 +31,43 @@ export function addToModalContent(id) {
     eventPointPlace.textContent = `${city.name}, ${country.name}, `;
     eventPointPlaceAddress.textContent = `${address.line1}`;
     eventFace.textContent = _embedded.attractions.map(e => e.name).join(', ');
+
+    function shortPriceInfo() {
+      if (priceRanges) {
+        for (const { type, currency, min, max } of priceRanges) {
+          if (type === 'standard' && min === max) {
+            return (firstPriceText.textContent = `${type} ${
+              min || max
+            } ${currency}`.toUpperCase());
+          } else if (type === 'standard') {
+            return (firstPriceText.textContent =
+              `${type} ${min}-${max} ${currency}`.toUpperCase());
+          }
+        }
+      }
+      return (firstPriceInfo.textContent = 'Currently price info is absent');
+    }
+
+    function optionPriceInfo() {
+      if (priceRanges) {
+        for (const { type, currency, min, max } of priceRanges) {
+          if (type === 'VIP' && min === max) {
+            return (secondPriceText.textContent = `${type} ${
+              min || max
+            } ${currency}`.toUpperCase());
+          } else if (type === 'VIP') {
+            return (firstPriceText.textContent =
+              `${type} ${min}-${max} ${currency}`.toUpperCase());
+          }
+        }
+      }
+      secondPriceItem.classList.add('visually-hidden');
+    }
+
+    shortPriceInfo();
+    optionPriceInfo();
+
+    buyTicketBtn.forEach(btn => (btn.href = url));
+    //infoObj.getEvents().then(r => console.log(r));
   });
 }
-
-addToModalContent;
