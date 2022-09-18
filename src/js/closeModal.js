f3b0e6e3c2c1738a0c210b42ec87e7c7a4612a6f
Лілія <lily.fed85@gmail.com>
2022-09-18 13:54:01 +0300

    refactor modal js

--- src/js/closeModal.js
+++ src/js/closeModal.js
@@ -1,21 +1,14 @@
 import { addToModalContent } from './openModal';
-const modalBtnOpen = document.querySelector('.js-open-modal-team');
-const backdrop = document.querySelector('.js-backdrop');
-const backdropModal = document.querySelector('.js-modal-backdrop');
-const elementModal = document.querySelector('.team-modal');
-const modalCloseBtn = document.querySelector('.modal-button');
-const eventModalCloseBtn = document.querySelector('.close-button');
-const eventsList = document.querySelector('.eventcards__list');
+import refs from './refs';
 
-//! Открытие модалки
-modalBtnOpen.addEventListener('click', function (e) {
-  document.body.classList.add('no-scroll');
-  elementModal.classList.remove('visually-hidden');
-  backdrop.classList.remove('visually-hidden');
-  window.addEventListener('keydown', onModalCloseKey);
-});
+refs.teamModalOpenBtn.addEventListener('click', onTeamBtnClick);
+refs.teamModalCloseBtn.addEventListener('click', remove);
+refs.teamModalBackdrop.addEventListener('click', remove);
 
-eventsList.addEventListener('click', onEventClick);
+refs.eventModalCloseBtn.addEventListener('click', closeBtnClick);
+refs.eventModalBackdrop.addEventListener('click', closeBtnClick);
+refs.eventsList.addEventListener('click', onEventClick);
+
 //! Открывает модалку карточки
 function onEventClick(e) {
   const card = e.target.closest('li');
@@ -23,9 +16,9 @@ function onEventClick(e) {
     return;
   }
   document.body.classList.add('no-scroll');
-  backdropModal.classList.remove('visually-hidden');
+  refs.eventModalBackdrop.classList.remove('visually-hidden');
   window.addEventListener('keydown', onModalCloseKey);
-  // Запустить функцию, которая добавляет данные в разметку
+
   addToModalContent(card.attributes.id.textContent);
 }
 
@@ -37,19 +30,18 @@ function onModalCloseKey(e) {
   }
 }
 
-modalCloseBtn.addEventListener('click', remove);
-eventModalCloseBtn.addEventListener('click', closeBtnClick);
-
-backdrop.addEventListener('click', remove);
-backdropModal.addEventListener('click', closeBtnClick);
-
 function remove() {
   document.body.classList.remove('no-scroll');
-  elementModal.classList.add('visually-hidden');
-  backdrop.classList.add('visually-hidden');
+  refs.teamModalBackdrop.classList.add('visually-hidden');
 }
 
 function closeBtnClick() {
   document.body.classList.remove('no-scroll');
-  backdropModal.classList.add('visually-hidden');
+  refs.eventModalBackdrop.classList.add('visually-hidden');
+}
+
+function onTeamBtnClick(e) {
+  document.body.classList.add('no-scroll');
+  refs.teamModalBackdrop.classList.remove('visually-hidden');
+  window.addEventListener('keydown', onModalCloseKey);
 }
--- src/js/refs.js
+++ src/js/refs.js
@@ -8,4 +8,18 @@ export default {
   //for event modal
   eventModalBackdrop: document.querySelector('.js-modal-backdrop'),
   eventModalCloseBtn: document.querySelector('.close-button'),
+  //   для записи в модалку
+  smallPic: document.querySelector('.modal__small-pic'),
+  poster: document.querySelector('.modal__big-pic'),
+  eventInfo: document.querySelector('.js-info'),
+  eventTimedate: document.querySelector('.js-when'),
+  eventPointPlace: document.querySelector('.js-where'),
+  eventMapPoint: document.querySelector('.modal__map-point'),
+  eventFace: document.querySelector('.js-who'),
+  firstPriceInfo: document.querySelector('.js-first-price'),
+  firstPriceText: document.querySelector('.js-first-price .js-price-text'),
+  secondPriceItem: document.querySelector('.js-second-priceItem'),
+  secondPriceText: document.querySelector('.js-second-price .js-price-text'),
+  buyTicketBtn: document.querySelectorAll('.modal__list-btn'),
+  moreAboutEvent: document.querySelector('.moreinfo-link'),
 };
