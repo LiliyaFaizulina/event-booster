3c96e221765d8607236e26fe00600d4715767b62
BigSiCorp <si.kocorp@gmail.com>
2022-09-18 22:38:29 +0300

    MW_JS_07

--- src/js/openModal.js
+++ src/js/openModal.js
@@ -12,46 +12,130 @@ export function addToModalContent(id) {
     const posterBestSize = images.filter(
       image => image.ratio === '3_2' || image.ratio === '4_3'
     );
-    refs.poster.src = posterBestSize.find(
+    const poster = posterBestSize.find(
       image => image.height === Math.max(...posterBestSize.map(e => e.height))
     ).url;
 
-    smallPic.src = images[0].url;
-    eventInfo.textContent = info ? info : name;
-    eventTimedate.textContent = `${dates.start.localDate} ${
+    const eventTimedate = `${dates.start.localDate} ${
       dates.start.localTime ? dates.start.localTime.slice(0, 5) : ''
     } ${dates.timezone ? dates.timezone : ''}`;
-    eventPointPlace.textContent = `${city.name}, ${country.name}, ${
+    const eventPointPlace = `${city.name}, ${country.name}, ${
       address.line1 ? address.line1 : ''
     }`;
-    eventMapPoint.href = `https://www.google.com/maps/search/${location.latitude}+${location.longitude}`;
-    eventFace.textContent = _embedded.attractions.map(e => e.name).join(', ');
-    buyTicketBtn.forEach(btn => (btn.href = url));
-    moreAboutEvent.href = `https://www.google.com/search?q=${eventFace.textContent}+${city.name}+${dates.start.localDate}`;
+    const eventMapPoint = `https://www.google.com/maps/search/${location.latitude}+${location.longitude}`;
+    const eventFace = _embedded.attractions.map(e => e.name).join(', ');
+    const moreAboutEvent = `https://www.google.com/search?q=${eventFace}+${city.name}+${dates.start.localDate}`;
+
+    let firstPriceText = '';
+    let secondPriceText = '';
+    let hiddenClass = '';
 
     if (priceRanges) {
       priceRanges.forEach(({ type, currency, min, max }) => {
         if (type === 'standard' && min === max) {
-          firstPriceText.textContent = `${type} ${
-            min || max
-          } ${currency}`.toUpperCase();
+          firstPriceText = `${type} ${min || max} ${currency}`.toUpperCase();
         } else if (type === 'standard') {
-          firstPriceText.textContent =
-            `${type} ${min}-${max} ${currency}`.toUpperCase();
+          firstPriceText = `${type} ${min}-${max} ${currency}`.toUpperCase();
         } else if (type === 'VIP' && min === max) {
-          secondPriceText.textContent = `${type} ${
-            min || max
-          } ${currency}`.toUpperCase();
+          secondPriceText = `${type} ${min || max} ${currency}`.toUpperCase();
         } else if (type === 'VIP') {
-          firstPriceText.textContent =
-            `${type} ${min}-${max} ${currency}`.toUpperCase();
+          secondPriceText = `${type} ${min}-${max} ${currency}`.toUpperCase();
         } else {
-          secondPriceItem.classList.add('visually-hidden');
+          hiddenClass = 'visually-hidden';
         }
       });
     } else {
-      firstPriceInfo.textContent =
+      firstPriceText =
         'Currently price info is absent. Click "Buy tickets" for more information';
+      hiddenClass = 'visually-hidden';
     }
+    document.querySelector('.js-event-modal').innerHTML = modalMarkup(
+      poster,
+      info,
+      name,
+      eventTimedate,
+      eventMapPoint,
+      eventPointPlace,
+      eventFace,
+      firstPriceText,
+      url,
+      secondPriceText,
+      moreAboutEvent,
+      hiddenClass
+    );
   });
+}
+
+function modalMarkup(
+  poster,
+  info,
+  name,
+  eventTimedate,
+  eventMapPoint,
+  eventPointPlace,
+  eventFace,
+  firstPriceText,
+  url,
+  secondPriceText,
+  moreAboutEvent,
+  hiddenClass
+) {
+  const markup = `<img src="${poster}" alt="small-pic" class="modal__small-pic">
+        <div class="modal__list">
+            <div class="modal__card-poster">
+                <img src="${poster}" alt="big-pic" class="modal__big-pic">
+            </div>
+            <div class="modal__description">
+                <ul>
+                    <li class="modal__list-info">
+                        <h3 class="modal__item-title">INFO</h3>
+                        <p class="modal__item-text js-info">${
+                          info ? info : name
+                        }</p>
+                    </li>
+                    <li class="modal__list-info">
+                        <h3 class="modal__item-title">WHEN</h3>
+                        <p class="modal__item-text js-when">${eventTimedate}</p>
+                    </li>
+                    <li class="modal__list-info">
+                        <h3 class="modal__item-title">WHERE</h3>
+                        <a class="modal__map-point" href="${eventMapPoint}" target="_blanc">
+                            <p class="modal__item-text">
+                            <svg class="modal__icon-location" width="30">
+                                <use href="./images/svg/sprite.svg#icon-location2"></use>
+                            </svg><span class="js-where">${eventPointPlace}</span></p></a>                        
+                    </li>
+                    <li class="modal__list-info">
+                        <h3 class="modal__item-title">WHO</h3>
+                        <p class="modal__item-text js-who">${eventFace}</p>
+                    </li>
+                    <li class="modal__list-info">
+                        <h3 class="modal__item-title">PRICES</h3>
+                        <ul class="modal__price-list">
+                            <li class="modal__price-item">
+                                <p class="modal__item-text js-first-price">
+                                        <svg class="modal__icon-code" width="30">
+                                            <use href="./images/svg/sprite.svg#ic_ticket"></use>
+                                        </svg>
+                                    <span class="js-price-text">${firstPriceText}</span>
+                                </p>
+                                <a class="modal__list-btn" href="${url}" target="_blanc">BUY TICKETS</a>
+                            </li>
+                            <li class="modal__price-item js-second-priceItem ${hiddenClass}">
+                                <p class="modal__item-text js-second-price">
+                                    <svg class="modal__icon-code" width="30">
+                                            <use href="./images/svg/sprite.svg#ic_ticket"></use>
+                                        </svg>
+                                    <span class="js-price-text">${secondPriceText}</span>
+                                </p>
+                                <a class="modal__list-btn" href="${url}" target="_blanc">BUY TICKETS</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <a class="moreinfo-link" target="_blank" href="${moreAboutEvent}">MORE ABOUT THIS
+            EVENT</a>`;
+  return markup;
 }
